9f79d4501ee2ec5010fbe495a79d60199c07a01d
KostyanKriukov <kostyan123452003@gmail.com>
2022-05-13 13:13:56 +0300

    Виконав Завдання 8.1-8.3

---
+++ lab08/prj/ModuleKriukov.h
@@ -0,0 +1,15 @@
+#ifndef MODULEKRIUKOV_H_INCLUDED
+#define MODULEKRIUKOV_H_INCLUDED
+
+#include <cmath>
+#include <math.h>
+#include <iostream>
+
+float s_calculation(float x, float y, float z){
+    float S;
+    S = pow((3 * sin(sqrt((12 * pow(x, 2)) / (log(x - 3))))), 2) + 1/2 * z;
+    return S;
+
+}
+
+#endif // MODULEKRIUKOV_H_INCLUDED
